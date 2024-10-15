e8add298aff80475d1e9054b8361bcc8dbf81b18
lazyihui <chengbaozi6@qq.com>
2024-10-15 14:59:54 +0800

    正常

--- Assets/0Scr_Runtime/Game_Business/Entity/PlaneEntity.cs
+++ Assets/0Scr_Runtime/Game_Business/Entity/PlaneEntity.cs
@@ -1,14 +1,18 @@
 using System;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 
 public class PlaneEntity : MonoBehaviour {
 
     [SerializeField] GameObject ray;
 
+    [SerializeField] XRRayInteractor rayInteractor;
+
+
     public int id;
 
-
+    public Vector3 endPos;
     public float moveSpeed;
 
 
@@ -16,6 +20,40 @@ public class PlaneEntity : MonoBehaviour {
         moveSpeed = 5.5f;
         ray.gameObject.SetActive(false);
     }
+
+    public void Update() {
+        endPos = rayInteractor.rayEndPoint;
+
+        rayInteractor.raycastMask = LayerMask.GetMask("Plane");
+
+        // rayInteractor.hitClosestOnly;
+
+        bool has = rayInteractor.TryGetCurrent3DRaycastHit(out RaycastHit hit,out int raycastEndpointIndex);
+
+
+
+
+        if (has) {
+        
+            Debug.Log(raycastEndpointIndex);
+            Debug.Log("hit:" + hit.point + " " + hit.collider.gameObject.name + hit.transform.name);
+        }
+
+        rayInteractor.raycastTriggerInteraction = QueryTriggerInteraction.Collide;
+
+
+
+
+        // if (rayInteractor.TryGetHitInfo(out RaycastHit hitInfo)) {
+        //     {
+        //         Debug.Log("hitInfo:" + hitInfo.point);
+        //     }
+        // }
+    }
+
+
+    // 碰撞检测
+   
 
     public void ShowRay() {
         ray.gameObject.SetActive(true);
